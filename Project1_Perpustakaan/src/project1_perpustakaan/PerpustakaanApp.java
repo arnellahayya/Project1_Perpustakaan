400d68fa56ca99d98de20a019436ac7b16f51435
arnellahayya <arnellahayya25@gmail.com>
2023-11-02 15:34:34 +0700

    2

--- Project1_Perpustakaan/src/project1_perpustakaan/PerpustakaanApp.java
+++ Project1_Perpustakaan/src/project1_perpustakaan/PerpustakaanApp.java
@@ -38,11 +38,11 @@ public class PerpustakaanApp extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 385, Short.MAX_VALUE)
+            .addGap(0, 425, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 314, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
